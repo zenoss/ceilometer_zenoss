99c83c0564dc6447653d29a33518cf4f7a65fc64
Josh Wilmes <jwilmes@zenoss.com>
2014-10-09 21:55:23 +0000

    Initial commit

---
+++ ceilometer_zenoss/dispatcher/zenoss.py
@@ -0,0 +1,384 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##############################################################################
+
+from oslo.config import cfg
+
+from ceilometer import dispatcher
+from ceilometer.openstack.common import log
+
+from kombu import Connection
+from kombu.entity import Exchange
+from kombu.messaging import Producer
+import eventlet
+from eventlet import pools
+from eventlet import semaphore
+
+import datetime
+import os
+import os.path
+import socket
+import sys
+import time
+
+LOG = log.getLogger(__name__)
+
+zenoss_dispatcher_opts = [
+    cfg.StrOpt('zenoss_device',
+               default=None,
+               help='Zenoss device name for this openstack environment.'),
+    cfg.StrOpt('amqp_hostname',
+               default=None,
+               help='Zenoss AMQP Host'),
+    cfg.IntOpt('amqp_port',
+               default=None,
+               help='Zenoss AMQP Port'),
+    cfg.StrOpt('amqp_userid',
+               default=None,
+               help='Zenoss AMQP UserID'),
+    cfg.StrOpt('amqp_password',
+               default=None,
+               help='Zenoss AMQP Password'),
+    cfg.StrOpt('amqp_virtual_host',
+               default=None,
+               help='Zenoss AMQP Virtual Host'),
+    cfg.IntOpt('amqp_max_retries',
+               default=5,
+               help='Maximum number of times to retry when (re)connecting to AMQP.'),
+    cfg.IntOpt('amqp_retry_interval_start',
+               default=1,
+               help='Seconds to sleep when retrying'),
+    cfg.IntOpt('amqp_retry_interval_step',
+               default=1,
+               help='Seconds to add to the sleep interval on each retry)'),
+    cfg.IntOpt('amqp_retry_interval_max',
+               default=5,
+               help='Maximum number of seconds to sleep between retries'),
+    cfg.IntOpt('zenoss_heartbeat_interval',
+               default=30,
+               help='Seconds to sleep between heartbeat messages sent to zenoss.'),
+]
+
+cfg.CONF.register_opts(zenoss_dispatcher_opts, group="dispatcher_zenoss")
+
+
+# Basic connection pooling for our amqp producers and sessions, used for sending
+# AMQP messages to zenoss.
+
+class AMQPConnection(object):
+
+    conf = None
+    connection = None
+    producer = None
+    needs_reconnect = False
+
+    def __init__(self, conf, exchange):
+        self.conf = conf
+        self.exchange = exchange
+        self.needs_reconnect = False
+        self.reconnect()
+
+    def reconnect(self):
+        LOG.info("Opening new AMQP connection to amqp://%s@%s:%s%s (%s)" % (
+            self.conf.amqp_userid, self.conf.amqp_hostname, self.conf.amqp_port, self.conf.amqp_virtual_host, self.exchange.name))
+
+        if self.connection:
+            self.connection.release()
+
+        try:
+            self.connection = Connection(
+                hostname=self.conf.amqp_hostname,
+                userid=self.conf.amqp_userid,
+                password=self.conf.amqp_password,
+                virtual_host=self.conf.amqp_virtual_host,
+                port=self.conf.amqp_port)
+
+            channel = self.connection.channel()   # get a new channel
+            self.producer = Producer(channel, self.exchange, auto_declare=False)
+        except Exception, e:
+            LOG.error("Error opening AMQP connection: %s" % e)
+            self.needs_reconnect = True
+
+
+class AMQPPool(pools.Pool):
+    def __init__(self, conf, *args, **kwargs):
+        self.conf = conf
+        super(AMQPPool, self).__init__(*args, **kwargs)
+
+    def create(self):
+        exchange = Exchange('zenoss.openstack.ceilometer', type='topic')
+        return AMQPConnection(self.conf, exchange)
+
+_pool_create_sem = semaphore.Semaphore()
+
+
+class Heartbeat(object):
+
+    disable_until = None
+
+    def __init__(self, conf):
+        self.conf = conf
+        exchange = Exchange('zenoss.openstack.heartbeats', type='topic')
+        self.connection = AMQPConnection(self.conf, exchange)
+
+        self.hostname = socket.gethostname()
+        self.processname = os.path.basename(sys.argv[0])
+        self.processid = os.getpid()
+
+        # Start sending heartbeats to zenoss
+        def heartbeat_loop():
+            conf = self.conf
+
+            while True:
+                if self.connection.needs_reconnect:
+                    self.connection.reconnect()
+                    if self.connection.needs_reconnect:
+                        LOG.error("Unable to establish AMQP connection, unable to send heartbeats at this time.")
+                        self.disable_for(conf.amqp_retry_interval_start)
+                        return
+                    self.reenable()
+
+                if self.enabled:
+                    self.send_heartbeat()
+                eventlet.sleep(conf.zenoss_heartbeat_interval)
+
+        self._heartbeat = eventlet.spawn(heartbeat_loop)
+
+    @property
+    def enabled(self):
+        if self.disable_until is None:
+            return True
+        else:
+            if self.disable_until < datetime.datetime.now():
+                self.reenable()
+                return True
+            return False
+
+    def disable_for(self, seconds):
+        self.disable_until = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+
+    def reenable(self):
+        self.disable_until = None
+
+    def send_heartbeat(self):
+        conf = self.conf
+
+        routing_key = 'zenoss.openstack.heartbeat.%s.%s.%s' % (
+            conf.zenoss_device,
+            self.hostname,
+            self.processname
+        )
+
+        heartbeat_message = dict(
+            timestamp=int(time.time()),
+            hostname=self.hostname,
+            processname=self.processname,
+            processid=self.processid
+        )
+
+        def errback(exc, interval):
+            LOG.warning("Couldn't publish heartbeat message: %r. Retry in %ds" % (exc, interval))
+
+        publish_with_retry = self.connection.connection.ensure(
+            self.connection.producer,
+            self.connection.producer.publish,
+            errback=errback,
+            max_retries=conf.amqp_max_retries,
+            interval_start=conf.amqp_retry_interval_start,
+            interval_step=conf.amqp_retry_interval_step,
+            interval_max=conf.amqp_retry_interval_max,
+            )
+
+        LOG.info("Sending heartbeat to %s" % routing_key)
+        publish_with_retry(
+            heartbeat_message,
+            serializer='json',
+            routing_key=routing_key,
+            headers={'x-message-ttl': 600000}  # 10 minutes
+        )
+
+
+class ZenossDispatcher(dispatcher.Base):
+    '''
+
+    [dispatcher_zenoss]
+
+    # Name of the device in zenoss for this openstack environment
+    zenoss_device=myopenstack
+    amqp_hostname=zenosshost
+    amqp_port=5672
+    amqp_userid=zenoss
+    amqp_password=zenoss
+    amqp_virtual_host=/zenoss
+
+    To enable this dispatcher, the following section needs to be present in
+    ceilometer.conf file
+
+    [collector]
+    dispatchers = zenoss
+
+    (Note that other dispatchers may be listed on their own lines as well,
+     such as dispatchers = database)
+    '''
+
+    pool = None
+    heartbeat = None
+    disable_until = None
+
+    def __init__(self, conf):
+        super(ZenossDispatcher, self).__init__(conf)
+
+        missing_cfg = set()
+        for required_cfg in ('zenoss_device', 'amqp_hostname', 'amqp_port',
+                             'amqp_userid', 'amqp_password',
+                             'amqp_virtual_host'):
+
+            if getattr(self.conf.dispatcher_zenoss, required_cfg, None) is None:
+                missing_cfg.add(required_cfg)
+
+        if len(missing_cfg):
+            LOG.error("Zenoss dispatcher disabled due to missing required configuration: %s" %
+                      (", ".join(missing_cfg)))
+            self.disable_permanently()
+            return
+
+        self.reenable()
+
+        # Create the connection pool.
+        try:
+            with _pool_create_sem:
+                # Just in case, make sure only one thread tries to create the
+                # connection pool for this dispatcher.
+                if not ZenossDispatcher.pool:
+                    ZenossDispatcher.pool = AMQPPool(self.conf.dispatcher_zenoss)
+
+                # Create the heartbeat thread while we're at it too.
+                if not ZenossDispatcher.heartbeat:
+                    ZenossDispatcher.heartbeat = Heartbeat(self.conf.dispatcher_zenoss)
+        except Exception, e:
+            LOG.error("Exception during initialization: %s" % e)
+            raise e
+
+    @property
+    def enabled(self):
+        if self.disable_until is None:
+            return True
+        else:
+            if self.disable_until < datetime.datetime.now():
+                self.reenable()
+                return True
+            return False
+
+    def disable_for(self, seconds):
+        self.disable_until = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+
+    def disable_permanently(self):
+        self.disable_until = datetime.datetime.max
+
+    def reenable(self):
+        self.disable_until = None
+
+    def publish(self, amqp, routing_key, data):
+        def errback(exc, interval):
+            LOG.warning("Couldn't publish message: %r. Retry in %ds" % (exc, interval))
+
+        conf = self.conf.dispatcher_zenoss
+
+        publish_with_retry = amqp.connection.ensure(
+            amqp.producer,
+            amqp.producer.publish,
+            errback=errback,
+            max_retries=conf.amqp_max_retries,
+            interval_start=conf.amqp_retry_interval_start,
+            interval_step=conf.amqp_retry_interval_step,
+            interval_max=conf.amqp_retry_interval_max,
+            )
+
+        publish_with_retry(data,
+                           serializer='json',
+                           routing_key=routing_key)
+
+    def record_metering_data(self, data):
+        conf = self.conf.dispatcher_zenoss
+
+        if not self.enabled:
+            return
+
+        if not isinstance(data, list):
+            data = [data]
+
+        with self.pool.item() as amqp:
+            if amqp.needs_reconnect:
+                amqp.reconnect()
+                if amqp.needs_reconnect:
+                    LOG.error("Unable to establish AMQP connection, unable to record metering data at this time.")
+                    self.disable_for(conf.amqp_retry_interval_start)
+                    return
+
+            for data_item in data:
+                routing_key = ".".join([
+                    'zenoss',
+                    'openstack',
+                    self.conf.dispatcher_zenoss.zenoss_device,
+                    'meter',
+                    data_item['counter_name'],
+                    data_item['resource_id']
+                ])
+
+                LOG.debug("Publishing message to %s" % (routing_key))
+
+                self.publish(amqp, routing_key, {
+                    'device': self.conf.dispatcher_zenoss.zenoss_device,
+                    'type': 'meter',
+                    'data': data_item
+                })
+
+    def record_events(self, events):
+        conf = self.conf.dispatcher_zenoss
+
+        LOG.info("record_events called (events=%s)" % events)
+
+        if not self.enabled:
+            return
+
+        if not isinstance(events, list):
+            events = [events]
+
+        with self.pool.item() as amqp:
+            if amqp.needs_reconnect:
+                amqp.reconnect()
+                if amqp.needs_reconnect:
+                    LOG.error("Unable to establish AMQP connection, unable to record event data at this time.")
+                    self.disable_for(conf.amqp_retry_interval_start)
+                    return
+
+            for event in events:
+                routing_key = ".".join([
+                    'zenoss',
+                    'openstack',
+                    self.conf.dispatcher_zenoss.zenoss_device,
+                    'event',
+                    event.event_type
+                ])
+
+                LOG.debug("Publishing message to %s" % (routing_key))
+
+                self.publish(amqp, routing_key, {
+                    'device': self.conf.dispatcher_zenoss.zenoss_device,
+                    'type': 'event',
+                    'data': event.as_dict()
+                })
---
+++ setup.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##############################################################################
+
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requires = [
+    'ceilometer',
+    'kombu',
+    'eventlet'
+]
+
+
+setup(
+    name='ceilometer_zenoss',
+    packages=find_packages(),
+    package_data={
+        '': ['*.yaml'],
+    },
+    entry_points={
+        'ceilometer.dispatcher': 'zenoss = ceilometer_zenoss.dispatcher.zenoss:ZenossDispatcher'
+    },
+
+    version='0.9.3-dev',
+    description="Ceilometer dispatcher plugin to ship data to Zenoss.",
+    long_description=read('README.rst'),
+
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Environment :: OpenStack',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Topic :: System :: Monitoring',
+        'Natural Language :: English',
+    ],
+
+    keywords="openstack ceilometer zenoss",
+    author='Zenoss, Inc.',
+    author_email='support@zenoss.com',
+    url='http://github.com/zenoss/ceilometer_zenoss',
+    license=read('LICENSE'),
+
+    requires=requires,
+    install_requires=requires,
+    zip_safe=False
+)
