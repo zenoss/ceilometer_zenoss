866c6c44e64e1933a8bcb0a64000da27868cdd03
Josh Wilmes <jwilmes@zenoss.com>
2017-04-04 16:09:37 -0400

    Start 1.1.2 Hotfix: Version 1.1.1 -> 1.1.2

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             'zenoss = ceilometer_zenoss.dispatcher.zenoss:ZenossDispatcher',
     },
 
-    version='1.1.1',
+    version='1.1.2',
     description="Ceilometer dispatcher plugin to ship data to Zenoss.",
     long_description=read('README.rst'),
 
